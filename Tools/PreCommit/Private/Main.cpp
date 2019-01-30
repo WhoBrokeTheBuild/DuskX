e530a06d6ea4f503301cc2544c881c2fa0e13115
Stephen Lane-Walsh <sdl.slane@gmail.com>
2019-01-30 18:51:57 -0500

    Fixed PreCommit for linux, Added multithreading

--- Tools/PreCommit/Private/Main.cpp
+++ Tools/PreCommit/Private/Main.cpp
@@ -7,12 +7,15 @@
 #include <sstream>
 #include <regex>
 #include <array>
+#include <atomic>
+#include <thread>
 
 #include <experimental/filesystem>
 
 namespace fs = std::experimental::filesystem;
 
-std::string exec(const char* cmd) {
+std::string exec(const char* cmd)
+{
     std::array<char, 128> buffer;
     std::string result;
 #if defined(_WIN32)
@@ -32,49 +35,57 @@ std::string exec(const char* cmd) {
 int main(int argc, char** argv)
 {
     auto fileList = exec("git ls-tree -r master --name-only");
-    std::regex reFindTabs(R"((\t))");
-#if !defined(_WIN32)
-    std::regex reFindCarriageReturn(R"((\r))");
-#endif
-    std::regex reFindTrailingSpace(R"(([ \t]+(?=\n|$)))");
 
-    bool noTabs = true;
-    bool noCarriageReturns = true;
-    bool noTrailingSpace = true;
+    std::atomic_bool noTabs = true;
+    std::atomic_bool noCarriageReturns = true;
+    std::atomic_bool noTrailingSpace = true;
+
+    std::vector<std::thread> threads;
+
     std::stringstream ss(fileList);
     for (std::string filename; std::getline(ss, filename); ) {
-        if (fs::path(filename).filename() == "Makefile") {
-            continue;
-        }
+        threads.push_back(std::thread(
+            [&noTabs, &noCarriageReturns, &noTrailingSpace](std::string filename)
+            {
+                std::regex reFindTabs(R"((\t))");
+                std::regex reFindTrailingSpace(R"(([ \t]+(?=\n|$)))");
+#               if !defined(_WIN32)
+                    std::regex reFindCarriageReturn(R"((\r))");
+#               endif
 
-        std::error_code ec;
-        uintmax_t size = fs::file_size(filename, ec);
-        if (!ec) {
-            std::ifstream file(filename, std::ifstream::binary);
-            std::string data(size, '\0');
-            file.read(&data[0], size);
+                std::error_code ec;
+                uintmax_t size = fs::file_size(filename, ec);
+                if (!ec) {
+                    std::ifstream file(filename, std::ifstream::binary);
+                    std::string data(size, '\0');
+                    file.read(&data[0], size);
 
-            std::smatch sm;
-            std::regex_search(data, sm, reFindTabs);
-            if (!sm.empty()) {
-                fprintf(stderr, "File has tabs '%s'\n", filename.c_str());
-                noTabs = false;
-            }
+                    std::smatch sm;
+                    std::regex_search(data, sm, reFindTabs);
+                    if (!sm.empty()) {
+                        fprintf(stderr, "File has tabs '%s'\n", filename.c_str());
+                        noTabs = false;
+                    }
 
-#if !defined(_WIN32)
-            std::regex_search(data, sm, reFindCarriageReturn);
-            if (!sm.empty()) {
-                fprintf(stderr, "File has carriage returns '%s'\n", filename.c_str());
-                noCarriageReturns = false;
-            }
-#endif
+#                   if !defined(_WIN32)
+                        std::regex_search(data, sm, reFindCarriageReturn);
+                        if (!sm.empty()) {
+                            fprintf(stderr, "File has carriage returns '%s'\n", filename.c_str());
+                            noCarriageReturns = false;
+                        }
+#                   endif
 
-            std::regex_search(data, sm, reFindTrailingSpace, std::regex_constants::match_not_eol);
-            if (!sm.empty()) {
-                fprintf(stderr, "File has trailing whitespace '%s'\n", filename.c_str());
-                noTrailingSpace = false;
-            }
-        }
+                    std::regex_search(data, sm, reFindTrailingSpace, std::regex_constants::match_not_eol);
+                    if (!sm.empty()) {
+                        fprintf(stderr, "File has trailing whitespace '%s'\n", filename.c_str());
+                        noTrailingSpace = false;
+                    }
+                }
+        }, filename));
+    }
+
+    for (auto& t : threads) {
+        t.join();
     }
 
     if (noTabs && noCarriageReturns && noTrailingSpace) {
